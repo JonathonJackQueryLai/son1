9d701c9e90eef34a4fa3f27de616a416b6a04721
JonathonJackQueryLai <948666249@qq.com>
2020-02-28 16:40:08 +0800

    '登陆组件'

---
+++ src/components/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='Login'>
+      
+  </div>
+</template>
+
+<script scope>
+export default {
+  name: 'Login',
+  data(){
+    return {}
+  },
+  components: {},
+  created(){},
+  mounted(){},
+  methods: {}
+}
+</script>
+<style lang='scss' scoped>
+</style>
