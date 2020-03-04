5f8cc7599218e93636c962335336664489df2636
JonathonJackQueryLai <948666249@qq.com>
2020-03-04 08:48:45 +0800

    'axios获取新闻'

--- src/components/newf.vue
+++ src/components/newf.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="newf">
-    <!-- <button @click="bnget">get</button>
-    <button @click="bnpost">post</button>-->
+    <Button @click="bnget">get</Button>
+    <Button @click="bnpost">post</Button>
     <!-- <h2>{{string1}}</h2> -->
   </div>
 </template>
 
 <script scope>
 import axios from "axios";
+import Button from "vant/lib/button";
+import "vant/lib/button/style";
 export default {
   name: "newf",
   data() {
@@ -16,12 +18,10 @@ export default {
     };
   },
   components: {},
-  created() {
-    
-  },
+  created() {},
 
   mounted() {},
-  methods: {}
+  methods: { bnget() {}, bnpost() {} }
 };
 </script>
 <style lang='scss' scoped>
